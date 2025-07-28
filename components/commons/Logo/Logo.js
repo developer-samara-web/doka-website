150a85b603babcc6f00752eaf14cfbcaa926c2fa
developer-samara-web <developer.samara.web@gmail.com>
2025-07-28 04:10:48 +0400

    Add logo & update header

--- components/commons/Logo/Logo.js
+++ components/commons/Logo/Logo.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Logo() {
-	return <Image src="/logo.svg" alt="Logo" width={80} height={80} />
+	return <Image src="/logo.svg" alt="Logo" width={120} height={80} />
 }
